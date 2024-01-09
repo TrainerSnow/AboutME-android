7837744bec97155f8989147a9c0c58b6a6eb7aca
simon <simon.bela.bumiller@gmx.de>
2024-01-09 20:15:14 +0100

    migrate profile screen to new repos

--- feature/profile/src/main/java/com/aboutme/profile/screen/ProfileViewModel.kt
+++ feature/profile/src/main/java/com/aboutme/profile/screen/ProfileViewModel.kt
@@ -4,52 +4,34 @@ import androidx.lifecycle.viewModelScope
 import com.aboutme.core.data.AuthService
 import com.aboutme.core.data.repository.UserRepository
 import com.aboutme.core.domain.viewmodel.AboutMeViewModel
-import com.aboutme.core.model.Response
-import com.aboutme.core.model.ResponseError
 import com.aboutme.core.model.StreakInfo
-import com.aboutme.core.ui.model.description
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@Suppress("MemberVisibilityCanBePrivate")
 @HiltViewModel
 internal class ProfileViewModel @Inject constructor(
-    private val authService: AuthService,
-    private val userRepository: UserRepository
+    val userRepository: UserRepository,
+    val authService: AuthService
 ) : AboutMeViewModel<ProfileEvent, ProfileUiEvent, ProfileState>() {
 
     override val initialState = ProfileState()
 
-    init {
-        viewModelScope.launch {
-            //We use refresh to get the user data. We really don't need to refresh here, but its a simple workaround.
-            val response = authService.refresh()
-
-            //TODO: Streaks are currently not implemented server-side. Needs to be done
-            val streakInfo = StreakInfo(4, 11, 2, 3)
-
-            if (response is Response.Error) {
-                if (response.errors.first() == ResponseError.NotAuthorized) {
-                    triggerUiEvent(ProfileUiEvent.GoToAuth)
-                } else {
-                    updateState {
-                        it.copy(
-                            userState = ProfileUserState.Error(response.errors.first().description)
-                        )
-                    }
-                }
-            } else if (response is Response.Success) {
-                updateState {
-                    it.copy(
-                        userState = ProfileUserState.Success(
-                            user = response.data.user,
-                            streakInfo = streakInfo
-                        )
-                    )
-                }
-            }
+    val profileState: StateFlow<ProfileUserState> = userRepository
+        .getUser()
+        .map {
+            ProfileUserState.Success(it, StreakInfo(4, 11, 2, 3))
         }
-    }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ProfileUserState.Loading
+        )
 
     override fun handleEvent(event: ProfileEvent) = when (event) {
         ProfileEvent.ToggleNameInfoDialog -> updateState {
@@ -94,21 +76,13 @@ internal class ProfileViewModel @Inject constructor(
     private fun handleDeleteProfile() {
         triggerUiEvent(ProfileUiEvent.GoToAuth)
         viewModelScope.launch {
-            userRepository.deleteUser()
+            authService.deleteUser()
         }
     }
 
     private fun handleChangeName(event: ProfileEvent.ChangeName) {
         viewModelScope.launch {
-            val response = userRepository.updateUser(event.nameInfo)
-            if (response is Response.Success) {
-                updateState {
-                    it.copy(
-                        userState = ProfileUserState.Success(response.data, StreakInfo(4, 11, 2, 3)),
-                        uiState = it.uiState.copy(showNameChangeDialog = false)
-                    )
-                }
-            }
+            userRepository.updateUser(event.nameInfo)
         }
     }
 }
