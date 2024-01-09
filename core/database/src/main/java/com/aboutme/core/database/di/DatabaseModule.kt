74b3867994bb6ba411ec0570257328d405e792f9
simon <simon.bela.bumiller@gmx.de>
2024-01-09 16:14:07 +0100

    expose daos via hilt

--- core/database/src/main/java/com/aboutme/core/database/di/DatabaseModule.kt
+++ core/database/src/main/java/com/aboutme/core/database/di/DatabaseModule.kt
@@ -3,6 +3,11 @@ package com.aboutme.core.database.di
 import android.content.Context
 import androidx.room.Room
 import com.aboutme.core.database.base.AboutMeDatabase
+import com.aboutme.core.database.dao.DiaryDataDao
+import com.aboutme.core.database.dao.DreamDao
+import com.aboutme.core.database.dao.DreamDataDao
+import com.aboutme.core.database.dao.MoodDataDao
+import com.aboutme.core.database.dao.SleepDataDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -26,5 +31,35 @@ object DatabaseModule {
         )
         .fallbackToDestructiveMigration()
         .build()
+
+    @Provides
+    @Singleton
+    fun provideDiaryDataDao(
+        db: AboutMeDatabase
+    ): DiaryDataDao = db.diaryDataDao
+
+    @Provides
+    @Singleton
+    fun provideDreamDataDao(
+        db: AboutMeDatabase
+    ): DreamDataDao = db.dreamDataDao
+
+    @Provides
+    @Singleton
+    fun provideSleepDataDao(
+        db: AboutMeDatabase
+    ): SleepDataDao = db.sleepDataDao
+
+    @Provides
+    @Singleton
+    fun provideMoodDataDao(
+        db: AboutMeDatabase
+    ): MoodDataDao = db.moodDataDao
+
+    @Provides
+    @Singleton
+    fun provideDreamDao(
+        db: AboutMeDatabase
+    ): DreamDao = db.dreamDao
 
 }
