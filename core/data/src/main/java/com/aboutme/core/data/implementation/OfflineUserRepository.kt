b03e12806204c02cc7bc56d0028d2f720816551e
simon <simon.bela.bumiller@gmx.de>
2024-01-09 20:15:22 +0100

    add method to user repo

--- core/data/src/main/java/com/aboutme/core/data/implementation/OfflineUserRepository.kt
+++ core/data/src/main/java/com/aboutme/core/data/implementation/OfflineUserRepository.kt
@@ -3,7 +3,10 @@ package com.aboutme.core.data.implementation;
 import com.aboutme.core.data.repository.UserRepository
 import com.aboutme.core.database.dao.UserDao
 import com.aboutme.core.model.data.NameInfo
+import com.aboutme.core.model.data.UserData
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 
 internal class OfflineUserRepository(
     private val userDao: UserDao
@@ -13,4 +16,15 @@ internal class OfflineUserRepository(
         val current = userDao.getAll().first().firstOrNull() ?: return
         userDao.update(current.copy(nameInfo = nameInfo))
     }
+
+    override fun getUser(): Flow<UserData> = userDao
+        .getAll()
+        .map {
+            UserData(
+                it.first().nameInfo,
+                it.first().email,
+                it.first().createdAt,
+                it.first().updatedAt
+            )
+        }
 }
--- core/data/src/main/java/com/aboutme/core/data/repository/UserRepository.kt
+++ core/data/src/main/java/com/aboutme/core/data/repository/UserRepository.kt
@@ -4,6 +4,7 @@ import com.aboutme.core.data.AuthService
 import com.aboutme.core.model.Response
 import com.aboutme.core.model.data.NameInfo
 import com.aboutme.core.model.data.UserData
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Grants access to data related to the user that does not fit into the [AuthService]
@@ -15,5 +16,10 @@ interface UserRepository {
      * Updates the [NameInfo] associated with the current user
      */
     suspend fun updateUser(nameInfo: NameInfo)
+
+    /**
+     * Gets the current user
+     */
+    fun getUser(): Flow<UserData>
 
 }
