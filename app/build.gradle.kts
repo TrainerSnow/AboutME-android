e1ab2d5ba8b6cad6f2ca695c29e28e3b64e2943d
simon <simon.bela.bumiller@gmx.de>
2024-01-09 14:48:41 +0100

    set up hilt worker injection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,7 @@ dependencies {
 
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
+
+    implementation(libs.android.work)
+    implementation(libs.hilt.work)
 }
--- app/src/main/java/com/aboutme/AboutMeApplication.kt
+++ app/src/main/java/com/aboutme/AboutMeApplication.kt
@@ -1,9 +1,20 @@
 package com.aboutme;
 
 import android.app.Application
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 
 @HiltAndroidApp
-class AboutMeApplication : Application()
+class AboutMeApplication : Application(), Configuration.Provider {
+
+    @Inject
+    private lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
