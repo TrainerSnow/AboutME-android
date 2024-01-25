193a7ed824ffd49a191f9d3259b84b1b83c78542
simon <simon.bela.bumiller@gmx.de>
2024-01-25 16:26:09 +0100

    add a new progress model

---
+++ core/model/src/main/java/com/aboutme/core/model/daily/DailyDataProgress.kt
@@ -0,0 +1,22 @@
+package com.aboutme.core.model.daily
+
+data class DailyDataProgress(
+
+    /**
+     * The amount of completed tasks
+     */
+    val completed: Int,
+
+    /**
+     * The total amount of tasks to complete
+     */
+    val total: Int
+
+) {
+
+    init {
+        require(completed >= 0)
+        require(total >= 0)
+    }
+
+}
